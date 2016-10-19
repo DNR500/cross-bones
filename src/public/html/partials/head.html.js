55835a4ef9e9d8d082a124dca7fe3d3a2c4ba72a
NathanR <dnr500@gmail.com>
2016-10-19 03:27:01 +0100

    watch tasks updated and added for node and html generation

---
+++ src/public/html/partials/head.html.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Head = ({ title }) => (
+    <head>
+        <meta charSet='utf-8' />
+        <title>{title}</title>
+        <meta name='description' content='' />
+        <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet' />
+        <link href='css/main.css' rel='stylesheet' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
+    </head>
+)
+
+Head.propTypes = {
+    title: React.PropTypes.string.isRequired,
+};
+
+export default Head
---
+++ src/public/index.html.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from './html/partials/head.html'
+
+class Index extends React.Component {
+    render () {
+        return (
+            <html>
+            <Head title='Cross Bones Index Page' />
+            <body>
+                <div>Cross Bones Index</div>
+            </body>
+            </html>
+        )
+    }
+}
+
+export default Index
